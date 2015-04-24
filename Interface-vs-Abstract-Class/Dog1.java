b97eebb207c6bb6a40810b947d434c378e778d88
7jiazhangliu7 <77jiazhangliu77@gmail.com>
2015-04-24 16:48:57 -0500

    Create Dog1.java

---
+++ Interface-vs-Abstract-Class/Dog1.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Jiazhang_Liu on 4/24/2015.
+ */
+public class Dog1 extends Dog{
+    public Dog1(String name) {
+        super(name);
+    }
+
+    public void cry1(){
+        System.err.println("Wang!");
+    }
+}
